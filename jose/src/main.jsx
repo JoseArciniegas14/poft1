a3fd564bed9a6225c0c6039e423a4b41bd8e0f2c
Jose Arciniegas <132412300+JoseArciniegas14@users.noreply.github.com>
2024-03-08 20:57:43 +0000

    hola

---
+++ jose/src/main.jsx
@@ -0,0 +1,17 @@
+
+import App from './App.jsx'
+import './index.css'
+
+import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import * as ReactDOM from 'react-dom/client'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <ChakraProvider>
+    <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+  
+)
