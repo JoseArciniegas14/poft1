f2cf67323385092af85498f1361ac0b1400c0a33
Jose Arciniegas <132412300+JoseArciniegas14@users.noreply.github.com>
2024-03-27 14:36:58 +0000

    tamplate

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,32 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		daisyui: {
+			themes: [
+			  {
+				mytheme: {
+				
+	   "primary": "#0e7490",
+				
+	   "secondary": "#00fc7c",
+				
+	   "accent": "#ff0000",
+				
+	   "neutral": "#1a1d22",
+				
+	   "base-100": "#302b36",
+				
+	   "info": "#37d9ff",
+				
+	   "success": "#00f3c8",
+				
+	   "warning": "#facc15",
+				
+	   "error": "#d3003b",
+				},
+			  },
+			"coffee",],
+		  },
 	},
-	plugins: [require("daisyui"),],
+	plugins: [require("daisyui")],
 }
