94300fe0e6b1b7ed343daa71afb62e116a290976
Jose Arciniegas <132412300+JoseArciniegas14@users.noreply.github.com>
2024-04-17 11:12:58 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()]
+    site: 'https://josearciniegas14.github.io',
+  base: 'https://github.com/JoseArciniegas14/JoseArciniegas14.github.io',
 });
