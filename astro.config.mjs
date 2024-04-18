6a541b157dea343a6607e84ebf2dd28b822216f6
Jose Arciniegas <132412300+JoseArciniegas14@users.noreply.github.com>
2024-04-18 16:39:00 +0000

    Revert "Update astro.config.mjs"
    This reverts commit 94300fe0e6b1b7ed343daa71afb62e116a290976.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,4 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()]
-    site: 'https://josearciniegas14.github.io',
-  base: 'https://github.com/JoseArciniegas14/JoseArciniegas14.github.io',
 });
