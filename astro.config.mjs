eb3e68a7742955fc4f7453ffc61abd2fae584860
Jose Arciniegas <132412300+JoseArciniegas14@users.noreply.github.com>
2024-04-18 16:38:30 +0000

    Revert "fix error site line"
    This reverts commit b89ac6036dd703867cf5b0d25c6120a32c446517.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind()]
     site: 'https://josearciniegas14.github.io',
   base: 'https://github.com/JoseArciniegas14/JoseArciniegas14.github.io',
 });
